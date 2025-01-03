a9f4845617be323ff89f572c1e67702aa9c64dea
linux-leo <leo@catsconsultants.com>
2025-01-03 21:21:07 +0100

    Update train_gpt2.py

--- train_gpt2.py
+++ train_gpt2.py
@@ -199,6 +199,7 @@ class CausalSelfAttention(nn.Module):
         k = k.transpose(1, 2)  # (B, num_heads, T, head_dim)
         v = v.transpose(1, 2)  # (B, num_heads, T, head_dim)
         y = flex_attention(q, k, v, block_mask=block_mask, score_mod=lambda score, *args: -(score.square().log1p()))
+        y = 1.0 - y
         y = y.transpose(1, 2).contiguous().view_as(x) # re-assemble all head outputs side by side
         y = self.c_proj(y)
         return y
